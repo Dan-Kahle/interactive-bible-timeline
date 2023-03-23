51dcc91768d4995c8ccfbb61a0a811156c9ed760
Daniel Kahle <41868677+Dan-Kahle@users.noreply.github.com>
2023-03-22 21:41:30 -0500

    Add files via upload

---
+++ timeline.js
@@ -0,0 +1,30 @@
+const Timeline = {
+    People: [{
+        Name: "Adam",
+        Born: 0,
+        Age: 930,
+        Spouses: [
+            { Name: "Eve" }
+        ],
+        Note: "First Human",
+        Children: [
+            { Name: "Cain" },
+            { Name: "Abel" },
+            {
+                Name: "Seth",
+                FathersAgeAtBirth: 130,
+                Age: 912,
+                Children: [
+                    {
+                        Name: "Enosh",
+                        FathersAgeAtBirth: 105,
+                        Age: 905,
+                        Children: [
+                            {
+                                Name: "Kenan",
+                                FathersAgeAtBirth: 90,
+                            }]
+                    }]
+            }]
+    }]
+}
